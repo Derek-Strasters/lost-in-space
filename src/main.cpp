2d1be025c86eb00e8376253595575b02a756e511
derek-strasters <paracite.org@gmail.com>
2022-12-13 11:17:42 -0700

    Introduce arrays and switch case.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
 #include <Arduino.h>
 
 #define RED_LED 12
+#define DAH 300
+#define DIT 100
+#define ELEMENT_SEPARATOR 100
+#define LETTER_SEPARATOR 200
+#define SPACE 600
+#define END_MESSAGE 1800
 
 
 void morseSymbol(unsigned long holdTime, unsigned long releaseTime) {
@@ -10,20 +16,29 @@ void morseSymbol(unsigned long holdTime, unsigned long releaseTime) {
     delay(releaseTime);
 }
 
-void dash() {
-    morseSymbol(850, 250);
-}
-
-void dot() {
-    morseSymbol(250, 250);
-}
-
-void space() {
-    delay(750);
-}
-
-void end() {
-    delay(1750);
+template<size_t N>
+void printMorseString(char (&message)[N]) {
+    for (char element: message) {
+        switch (element) {
+            case '-':
+                morseSymbol(DAH, ELEMENT_SEPARATOR);
+                break;
+            case '.':
+                morseSymbol(DIT, ELEMENT_SEPARATOR);
+                break;
+            case ' ':
+                delay(LETTER_SEPARATOR);
+                break;
+            case '/':
+                delay(SPACE);
+                break;
+            case '\n':
+                delay(END_MESSAGE);
+                break;
+            default:
+                break;
+        }
+    }
 }
 
 void setup() {
@@ -33,31 +48,6 @@ void setup() {
 
 
 void loop() {
-    dash();
-    space();
-
-    dot();
-    dot();
-    dot();
-    dot();
-    space();
-
-    dot();
-    space();
-
-    dash();
-    dash();
-    dot();
-    space();
-
-    dot();
-    dash();
-    space();
-
-    dash();
-    dash();
-    space();
-
-    dot();
-    end();
+    char message[] = "- .... . / --. .- -- .\n";
+    printMorseString(message);
 }

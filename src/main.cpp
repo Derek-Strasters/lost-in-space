e1908e31a9a0e938c8aa8fbc1043e125a2e71492
derek-strasters <paracite.org@gmail.com>
2022-12-15 15:51:25 -0700

    Define a base time from which other times are derived.

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,15 @@
 #include <Arduino.h>
 
-#define RED_LED 12
-#define DAH 600
-#define DIT 200
-#define ELEMENT_SEPARATOR 200
-#define LETTER_SEPARATOR 600 // Should be 3x DIT, but assumed to be preceded by element separator.
-#define SPACE 1000 // Should be 7x DIT, but assumed to have element separators on either side.
-#define END_MESSAGE 2000
+#define RED_LED            12
+
+#define BASE_TIME          70
+
+#define DAH                ( BASE_TIME * 3 )
+#define DIT                ( BASE_TIME )
+#define ELEMENT_SEPARATOR  ( BASE_TIME )
+#define LETTER_SEPARATOR   ( BASE_TIME * 2 ) // Should be 3x BASE, assumed to be preceded by ELEMENT_SEPARATOR.
+#define SPACE              ( BASE_TIME * 5 ) // Should be 7x BASE, assumed to have element separators on either side.
+#define END_MESSAGE        ( BASE_TIME * 14 )
 
 
 void morseSymbol(unsigned long holdTime, unsigned long releaseTime) {
@@ -17,7 +20,7 @@ void morseSymbol(unsigned long holdTime, unsigned long releaseTime) {
 }
 
 template<size_t N>
-void printMorseChars(const char (&message)[N]) {
+void printMorseChars(__attribute__((unused)) const char (&message)[N]) {
     for (char element: message) {
         switch (element) {
             case '-':
@@ -78,5 +81,15 @@ void setup() {
 
 
 void loop() {
-    printStringInMorse("SOS. Hey come buy me 1 taco.");
+    printStringInMorse("Did you ever hear the tragedy of Darth Plagueis The Wise? I thought not. "
+                       "It’s not a story the Jedi would tell you. It’s a Sith legend. "
+                       "Darth Plagueis was a Dark Lord of the Sith, so powerful and so wise he "
+                       "could use the Force to influence the midichlorians to create life… "
+                       "He had such a knowledge of the dark side that he could even keep the ones "
+                       "he cared about from dying. The dark side of the Force is a pathway to many "
+                       "abilities some consider to be unnatural. He became so powerful… "
+                       "the only thing he was afraid of was losing his power, which eventually, "
+                       "of course, he did. Unfortunately, he taught his apprentice everything he knew, "
+                       "then his apprentice killed him in his sleep. Ironic. He could save others "
+                       "from death, but not himself.");
 }
